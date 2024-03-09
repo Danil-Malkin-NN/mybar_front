07cb04844196aa0bd97e9e7d4f40f441d1217b78
Danil <danilmalkin2015@gmail.com>
2024-03-09 10:49:24 +0300

    Добавил страничку инструментов

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IngredientList from './components/IngredientList';
+import InstrumentsList from "./components/InstrumentsList";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <div>
                 <Routes>
                     <Route path="/ingredients" element={<IngredientList />} />
+                    <Route path="/instruments" element={<InstrumentsList />} />
                     {/* Другие маршруты, если есть */}
                 </Routes>
             </div>
---
+++ src/components/InstrumentsList.js
@@ -0,0 +1,46 @@
+// instrumentList.js
+
+import React, {useState, useEffect} from 'react';
+import './instrumentList.css'; // импортируем файл стилей для карточек
+
+function instrumentList() {
+    const [instruments, setinstruments] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://mybar.dvmalkin.online/api/instruments/all')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch instruments');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setinstruments(data);
+            })
+            .catch(error => {
+                setError(error.message);
+            });
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div className="instrument-container">
+            <h1>instruments</h1>
+            <div className="instrument-grid">
+                {instruments.map(instrument => (
+                    <div key={instrument.id} className="instrument-card">
+                        <h3>{instrument.name}</h3>
+                        <p>{instrument.description}</p>
+                        {/* Другая информация об ингредиенте */}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default instrumentList;
