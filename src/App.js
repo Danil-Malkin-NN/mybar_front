e702d1816044329a8741dcc710820c6c1fc13d28
Danil <danilmalkin2015@gmail.com>
2024-03-09 10:21:39 +0300

    Пробник работы на сайте

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 // App.js
-import logo from './logo.svg';
-import './App.css';
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import IngredientList from './components/IngredientList'; // импортируем наш компонент
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IngredientList from './components/IngredientList';
 
 function App() {
-  return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/ingredients">
-              <IngredientList /> {/* отображаем список ингредиентов на этом маршруте */}
-            </Route>
-            {/* Другие маршруты, если есть */}
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/ingredients" element={<IngredientList />} />
+                    {/* Другие маршруты, если есть */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-
 
 export default App;
