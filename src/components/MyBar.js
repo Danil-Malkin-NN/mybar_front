8e36926f5122722c8440a1b1d92f4dbe020e6789
Danil <danilmalkin2015@gmail.com>
2024-03-10 12:46:12 +0300

    Добавил блок коктелей

--- src/components/MyBar.js
+++ src/components/MyBar.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from "react-router-dom";
 
 function MyBar() {
     const [ingredients, setIngredients] = useState([]);
+    const [cocktails, setCocktails] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         fetchIngredients();
+        myCocktails();
     }, []);
 
     const fetchIngredients = async () => {
@@ -18,11 +21,21 @@ function MyBar() {
         }
     };
 
+    const myCocktails = async () => {
+        try {
+            const response = await axios.get('http://mybar.dvmalkin.online/api/my/available/cocktails');
+            setCocktails(response.data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
     const handleDeleteIngredient = async (ingredientId) => {
         try {
             await axios.delete(`http://mybar.dvmalkin.online/api/my/ingredients/delete?ingredientsId=${ingredientId}`);
             // Обновляем список ингредиентов после удаления
             fetchIngredients();
+            myCocktails();
         } catch (error) {
             setError(error.message);
         }
@@ -32,7 +45,7 @@ function MyBar() {
         return <div>Error: {error}</div>;
     }
 
-    return (
+    return (<>
         <div>
             <h1>Мой бар</h1>
             <div className="ingredient-grid">
@@ -46,6 +59,18 @@ function MyBar() {
                 ))}
             </div>
         </div>
+        <div className="ingredient-container">
+            <h1>Cocktails</h1>
+            <div className="ingredient-grid">
+                {cocktails.map(cocktail => (
+                    <Link key={cocktail.id} to={`/cocktails/${cocktail.id}`} className="ingredient-card">
+                        <h3>{cocktail.name}</h3>
+                        <p>{cocktail.description}</p>
+                    </Link>
+                ))}
+            </div>
+        </div>
+        </>
     );
 }
 
