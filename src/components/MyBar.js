40b6a56969407e154b564f78f2a48fa6751b987d
Danil <danilmalkin2015@gmail.com>
2024-03-10 12:37:34 +0300

    Добавил кнопку удаления

--- src/components/MyBar.js
+++ src/components/MyBar.js
@@ -1,27 +1,32 @@
-// MyBar.js
-
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function MyBar() {
     const [ingredients, setIngredients] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-
-        fetch('http://mybar.dvmalkin.online/api/my/ingredients')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch ingredients from My Bar');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setIngredients(data);
-            })
-            .catch(error => {
-                setError(error.message);
-            });
+        fetchIngredients();
     }, []);
+
+    const fetchIngredients = async () => {
+        try {
+            const response = await axios.get('http://mybar.dvmalkin.online/api/my/ingredients');
+            setIngredients(response.data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    const handleDeleteIngredient = async (ingredientId) => {
+        try {
+            await axios.delete(`http://mybar.dvmalkin.online/api/my/ingredients/delete?ingredientsId=${ingredientId}`);
+            // Обновляем список ингредиентов после удаления
+            fetchIngredients();
+        } catch (error) {
+            setError(error.message);
+        }
+    };
 
     if (error) {
         return <div>Error: {error}</div>;
@@ -30,17 +35,16 @@ function MyBar() {
     return (
         <div>
             <h1>Мой бар</h1>
-            <ul>
-                <div className="ingredient-grid">
-                    {ingredients.map(ingredient => (
-                        <div key={ingredient.id} className="ingredient-card">
-                            <h3>{ingredient.name}</h3>
-                            <p>{ingredient.description}</p>
-                            {/* Другая информация об ингредиенте */}
-                        </div>
-                    ))}
-                </div>
-            </ul>
+            <div className="ingredient-grid">
+                {ingredients.map(ingredient => (
+                    <div key={ingredient.id} className="ingredient-card">
+                        <h3>{ingredient.name}</h3>
+                        <p>{ingredient.description}</p>
+                        {/* Другая информация об ингредиенте */}
+                        <button onClick={() => handleDeleteIngredient(ingredient.id)}>Удалить</button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
