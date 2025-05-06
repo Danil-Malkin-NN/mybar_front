1e9455a4b7f68849c70938922908181276d02e4f
Danil <danilmalkin2015@gmail.com>
2025-05-06 20:30:57 +0300

    Пропущенный URL

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -11,11 +11,12 @@ function IngredientList() {
     const [size, setSize] = useState(10);
     const [totalPages, setTotalPages] = useState(0);
 
+    const apiUrl = process.env.REACT_APP_API_URL;
 
     useEffect(() => {
         async function fetchIngredients() {
             try {
-                const response = await axios.get(`/api/ingredients?page=${page}&size=${size}`);
+                const response = await axios.get(`${apiUrl}/api/ingredients?page=${page}&size=${size}`);
                 setIngredients(response.data.content);
                 setTotalPages(response.data.totalPages);
             } catch (error) {
@@ -28,7 +29,7 @@ function IngredientList() {
 
     const handleSearch = async () => {
         try {
-            const response = await axios.get(`/api/ingredients/search?name=${searchTerm}`);
+            const response = await axios.get(`${apiUrl}/api/ingredients/search?name=${searchTerm}`);
             setIngredients(response.data);
         } catch (error) {
             setError(error.message);
@@ -53,7 +54,7 @@ function IngredientList() {
 
     const handleAddIngredient = async (ingredientId) => {
         try {
-            const response = await axios.post(`/api/my/ingredients/add?ingredientsId=${ingredientId}`);
+            const response = await axios.post(`${apiUrl}/api/my/ingredients/add?ingredientsId=${ingredientId}`);
             console.log('Ingredient added successfully:', response.data);
             // Можно обновить список ингредиентов после успешного добавления
         } catch (error) {
